e2f41aa953741b1f666fe817534e699bef11fca3
nicenike <81592241+nicenike@users.noreply.github.com>
2026-03-08 01:00:08 +0100

    Add files via upload

--- quiz_bot.py
+++ quiz_bot.py
@@ -345,3 +345,19 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+from flask import Flask
+import threading
+
+app_web = Flask(__name__)
+
+@app_web.route("/")
+def home():
+    return "Bot is running"
+
+def run_web():
+    app_web.run(host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    threading.Thread(target=run_web).start()
+    main()
